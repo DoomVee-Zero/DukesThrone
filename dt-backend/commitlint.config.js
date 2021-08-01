2f4cd39d2e7dadbe539f9c5e9f72463fffab3891
Roman Shchekotov <rom.shchekotov@gmail.com>
2021-08-01 17:33:35 +0200

    config: include commit examples

---
+++ dt-backend/commitlint.config.js
@@ -0,0 +1,60 @@
+const Rule = {
+  Level: {
+    DISABLED: 0,
+    WARNING: 1,
+    ERROR: 2,
+  },
+  Applicable: {
+    ALWAYS: "always",
+    NEVER: "never",
+  },
+};
+
+/**
+ * Commit Message Structure:
+ * <type>(<scope?>): <subject>
+ *
+ * <optional body>
+ *
+ * 1st Example:
+ * feat: add cool knight texture
+ *
+ * 2nd Example:
+ * feat(ipc): add discord integration
+ *
+ * 3nd Example:
+ * revert: backend changes
+ *
+ * Fixed breaking changes.
+ */
+
+const Configuration = {
+  rules: {
+    "header-max-length": [Rule.Level.WARNING, Rule.Applicable.ALWAYS, 72],
+    "header-case": [Rule.Level.WARNING, Rule.Applicable.ALWAYS, "lower-case"],
+    "scope-case": [Rule.Level.WARNING, Rule.Applicable.ALWAYS, "lower-case"],
+    "subject-case": [Rule.Level.WARNING, Rule.Applicable.ALWAYS, "lower-case"],
+    "type-enum": [
+      Rule.Level.ERROR,
+      Rule.Applicable.ALWAYS,
+      [
+        "feat",
+        "fix",
+        "doc",
+        "style",
+        "refactor",
+        "test",
+        "revert",
+        "bump",
+        "del",
+        "config",
+        "infra",
+      ],
+    ],
+    "type-case": [Rule.Level.WARNING, Rule.Applicable.ALWAYS, "lower-case"],
+    "type-empty": [Rule.Level.ERROR, Rule.Applicable.NEVER],
+  },
+  helpUrl: "https://gist.github.com/turbo/efb8d57c145e00dc38907f9526b60f17",
+};
+
+module.exports = Configuration;
---
+++ dt-frontend/README.md
@@ -0,0 +1,24 @@
+# dt-frontend
+
+## Project setup
+```
+pnpm install
+```
+
+### Compiles and hot-reloads for development
+```
+pnpm run serve
+```
+
+### Compiles and minifies for production
+```
+pnpm run build
+```
+
+### Lints and fixes files
+```
+pnpm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
