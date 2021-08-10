81b690e3ec445c0d54b363974028ca990a24722d
BuildTools <unconfigured@null.spigotmc.org>
2021-08-10 09:31:13 -0400

    fix: syntax

--- dt-backend/src/model/audit-log/audit-log.resolver.ts
+++ dt-backend/src/model/audit-log/audit-log.resolver.ts
@@ -2,7 +2,7 @@ import { Resolver, Query } from '@nestjs/graphql';
 import { AuditLog } from '../types/audit-log.type';
 import { AuditLogService } from './audit-log.service';
 
-@Resolver((of) => AuditLog
+@Resolver((of) => AuditLog)
 export class AuditLogResolver {
   constructor(private readonly auditlogService: AuditLogService) {}
 
