e1e9c62de52965f33f8993e35c5037164023a5f6
Paul Giovanni <pgiovanni1234@gmail.com>
2022-01-02 15:32:34 -0500

    add: app.service.ts get functions

--- dt-backend/src/app.module.ts
+++ dt-backend/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './model/user/user.module';
 import { AuditLogModule } from './model/audit-log/audit-log.module';
 import { EmpireModule } from './model/empire/empire.module';
-
 import { LoggerModule } from './services/logger/logger.module';
 import { StructureModule } from './model/structure/structure.module';
 import { FinanceModule } from './model/finance/finance.module';
@@ -15,24 +13,16 @@ import { WarLogEntryModule } from './model/war-log-entry/war-log-entry.module';
 @Module({
   imports: [
     UserModule,
-    GraphQLModule.forRoot({
-      autoSchemaFile: `${process.cwd() + '/src/schema.gql'}`,
-    }),
     AuditLogModule,
-
     EmpireModule,
-
     LoggerModule,
-
     StructureModule,
-
     FinanceModule,
-
     VaultTransactionModule,
-
-    WarLogEntryModule,
+    WarLogEntryModule
   ],
   controllers: [AppController],
   providers: [AppService],
+
 })
 export class AppModule {}
