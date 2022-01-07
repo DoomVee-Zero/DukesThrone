a4d502a9fec8a56de36100ec1b62bad69b3c43c7
Paul Giovanni <pgiovanni1234@gmail.com>
2022-01-06 21:21:55 -0500

    created: 4 requests for user module

--- dt-backend/src/model/user/user.controller.ts
+++ dt-backend/src/model/user/user.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, Post, Delete, Patch, Body} from "@nestjs/common";
+import {Controller, Get, Post, Delete, Patch, Body, Param} from "@nestjs/common";
 import { User } from '../types/user.type'
 import { UserService} from "./user.service";
 
@@ -7,14 +7,28 @@ export class UserController {
 
     constructor(private readonly userService: UserService) {}
 
+    @Post('createuser')
+    async createUser (@Body() userData: {username: string; mail: string; password: string }): Promise<User> {
+        return this.userService.createUser(userData);
+    }
 
     @Get(":id")
-    async getUser(): Promise<User> {
-        return this.userService.getUser();
+    async getUserById(@Param('id') id: string): Promise<User> {
+        return this.userService.User({id: id});
     }
 
-    @Post('createuser')
-    async createUser (@Body() userData: {name: string; email: string; password: string }): Promise<User> {
-        return this.userService.createUser();
+    @Patch('')
+    async updateUser(@Param('id') id: string): Promise<User> {
+        return this.userService.updateUser({
+            where: { id: id },
+            data: { },
+        });
     }
+
+
+    @Delete(':id')
+    async deleteUser(@Param('id') id: string): Promise<User> {
+        return this.userService.deleteUser({id: id});
+    }
+
 }
--- dt-backend/src/model/user/user.service.ts
+++ dt-backend/src/model/user/user.service.ts
@@ -1,4 +1,4 @@
-import {Injectable, Post} from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
 import { prisma } from '../../main';
 import { User } from '../types/user.type';
 import { Prisma } from '@prisma/client'
@@ -6,11 +6,9 @@ import { Prisma } from '@prisma/client'
 @Injectable()
 export class UserService {
 
-  async getUser(): Promise<User> {
+  async User(userWhereUniqueInput: Prisma.UserWhereUniqueInput): Promise<User> {
     return prisma.user.findUnique({
-      where: {
-        id
-      },
+      where: userWhereUniqueInput,
       include: {
         audit: true,
         empire: true,
@@ -30,6 +28,28 @@ export class UserService {
   async createUser(data: Prisma.UserCreateInput): Promise<User> {
     return prisma.user.create({
       data,
+      include: {
+        audit: true,
+        empire: true,
+      },
+    })
+  }
+
+  async updateUser(params: {where: Prisma.UserWhereUniqueInput; data: Prisma.UserUpdateInput;}): Promise<User> {
+    const { data, where } = params;
+    return prisma.user.update({
+      data,
+      where,
+      include: {
+        audit: true,
+        empire: true,
+      }
+    })
+  }
+
+  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return prisma.user.delete({
+      where,
       include: {
         audit: true,
         empire: true,
